c5e3480d275660767e8430f7e60fab6944bc8a52
dmitrobaz <dmitrobaz@gmail.com>
2021-12-22 15:59:59 +0200

    Updates

---
+++ precache-manifest.41257e296154ebd7a58ea55ea71a5899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8e205650caad325874dbc4e5d8c477",
+    "url": "/trainee-category/index.html"
+  },
+  {
+    "revision": "bc25b0da62841dcd288e",
+    "url": "/trainee-category/static/css/main.ae5c0312.chunk.css"
+  },
+  {
+    "revision": "1147be024bd6b9778879",
+    "url": "/trainee-category/static/js/2.752171dc.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/trainee-category/static/js/2.752171dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc25b0da62841dcd288e",
+    "url": "/trainee-category/static/js/main.7aa1d41e.chunk.js"
+  },
+  {
+    "revision": "6e5b637d662f16cd18f8",
+    "url": "/trainee-category/static/js/runtime-main.3d2dca8f.js"
+  },
+  {
+    "revision": "8d3f1bb513c8d5c210d1149e7b32af5b",
+    "url": "/trainee-category/static/media/avatar.8d3f1bb5.png"
+  }
+]);
